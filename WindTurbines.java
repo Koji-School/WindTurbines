98265f776ac77d019726f887d104126d8c061a0a
krdennis01 <krdennis01@bvsd.com>
2026-01-14 14:28:50 -0700

    setup

--- WindTurbines.java
+++ WindTurbines.java
@@ -1,1 +1,12 @@
 
+import java.util.Scanner; //for reading keyboard input and Files
+import java.io.File;
+import java.io.PrintWriter; //for printing to a file
+import java.io.IOException; //Exception that can be thrown by File and Scanner
+import java.util.ArrayList;
+
+public class WindTurbines
+{
+
+
+}
